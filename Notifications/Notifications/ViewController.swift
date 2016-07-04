1d9cb4822f10cbb8288463f60192d799f3e23470
yuxiaohua <hzyuxiaohua@corp.netease.com>
2016-07-04 11:44:54 +0800

    Add Project:Notificatios

---
+++ Notifications/Notifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by hzyuxiaohua on 16/7/4.
+//  Copyright © 2016年 X Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
