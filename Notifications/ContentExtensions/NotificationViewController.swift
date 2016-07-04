ce8f8e90105e83c134bf5603f4ff108afd73a86a
yuxiaohua <hzyuxiaohua@corp.netease.com>
2016-07-04 12:00:56 +0800

    Add Target:Notification Service Extensions & Content Extensions

---
+++ Notifications/ContentExtensions/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  ContentExtensions
+//
+//  Created by hzyuxiaohua on 16/7/4.
+//  Copyright © 2016年 X Co., Ltd. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
