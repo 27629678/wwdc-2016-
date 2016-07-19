f37961ff0dafacf194243225122cc5cd979df8ce
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2016-07-19 13:56:22 +0800

    添加自定义的ContentExtensionUI

--- Notifications/ContentExtensions/NotificationViewController.swift
+++ Notifications/ContentExtensions/NotificationViewController.swift
@@ -12,7 +12,9 @@ import UserNotificationsUI
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
-    @IBOutlet var label: UILabel?
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+    @IBOutlet weak var body: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +22,9 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
     }
     
     func didReceive(_ notification: UNNotification) {
-        self.label?.text = notification.request.content.body
+        self.label?.text = notification.request.content.title
+        self.subtitle?.text = notification.request.content.subtitle
+        self.body?.text = notification.request.content.body
     }
 
     func didReceive(_ response: UNNotificationResponse, completionHandler completion: (UNNotificationContentExtensionResponseOption) -> Void) {
